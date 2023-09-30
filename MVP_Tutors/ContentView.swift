87c2174772e04191fd599e8983e3efb5be991c48
namanNagelia <nagelia.naman@gmail.com>
2023-09-30 12:29:18 -0400

    Fixed colors

--- MVP_Tutors/ContentView.swift
+++ MVP_Tutors/ContentView.swift
@@ -2,26 +2,36 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var selectedTab = 1
+    @Environment(\.colorScheme) var colorScheme
+
     var body: some View {
         //Top Navigator test
-                HStack{
-                    Button(action: {}){
-                        Image(systemName: "gear").resizable().frame(width: 30.0, height: 30.0).foregroundColor(.black)
+        HStack {
+                    Button(action: {}) {
+                        Image(systemName: "gear")
+                            .resizable()
+                            .frame(width: 30.0, height: 30.0)
+                            .foregroundColor(colorScheme == .dark ? .white : .black)
                     }
                     Spacer()
                     
-                    Button(action:{}) {
-                        Image(systemName: "bell.fill").resizable().frame(width: 30.0, height: 30.0).foregroundColor(.black)
-                    
+                    Button(action: {}) {
+                        Image(systemName: "bell.fill")
+                            .resizable()
+                            .frame(width: 30.0, height: 30.0)
+                            .foregroundColor(colorScheme == .dark ? .white : .black)
                     }
                     
-                    Button(action: {}) { Image(systemName: "person.circle.fill").resizable().frame(width: 30, height: 30) } .padding(.trailing, 16).foregroundColor(.black)
+                    Button(action: {}) {
+                        Image(systemName: "person.circle.fill")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .padding(.trailing, 16)
+                            .foregroundColor(colorScheme == .dark ? .white : .black)
+                    }
                 }
                 .padding(.leading, 15.0)
                 .frame(height: 30.0)
-
-
-        
         Spacer()
 
         
