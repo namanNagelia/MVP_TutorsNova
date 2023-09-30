051389e44f9c7d9ff3c5023efc40823c21b7b63b
namanNagelia <nagelia.naman@gmail.com>
2023-09-30 12:24:38 -0400

    added icon and search bar

--- MVP_Tutors/ListingsView.swift
+++ MVP_Tutors/ListingsView.swift
@@ -1,17 +1,29 @@
-//
-//  ListingsView.swift
-//  MVP_Tutors
-//
-//  Created by Naman Nagelia on 9/26/23.
-//
-
-import Foundation
 import SwiftUI
 
 struct ListingsView: View {
+    @State private var searchText = ""
+
     var body: some View {
-        Text("Listings Page")
-            .font(.largeTitle)
-            .padding()
+        VStack {
+            // Search Bar
+            TextField("Search", text: $searchText)
+                .padding()
+                .background(Color(.systemGray6))
+                .cornerRadius(10)
+                .padding(.horizontal)
+            
+            // Display search results or content below the search bar
+            Text("You searched for: \(searchText)")
+                .padding()
+            
+            Spacer()
+        }
+        .navigationBarTitle("Search Example", displayMode: .inline)
+    }
+}
+
+struct ListingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
