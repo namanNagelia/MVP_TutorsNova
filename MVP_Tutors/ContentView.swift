4cf76be0b192bad0a7a157973486d774940454a1
namanNagelia <nagelia.naman@gmail.com>
2023-10-23 00:01:46 -0400

    Tested changes added

--- MVP_Tutors/ContentView.swift
+++ MVP_Tutors/ContentView.swift
@@ -1,79 +1,70 @@
 import SwiftUI
-
 struct ContentView: View {
     @State private var selectedTab = 1
     @Environment(\.colorScheme) var colorScheme
     @State private var isProfileView = false
     @State private var isAuthenticated = false
 
-
     var body: some View {
-        //Top Navigator test
-        HStack {
-                    Button(action: {}) {
-                        Image(systemName: "gear")
-                            .resizable()
-                            .frame(width: 30.0, height: 30.0)
-                            .foregroundColor(colorScheme == .dark ? .white : .black)
-                    }
-                    Spacer()
-                    
-                    Button(action: {}) {
-                        Image(systemName: "bell.fill")
-                            .resizable()
-                            .frame(width: 30.0, height: 30.0)
-                            .foregroundColor(colorScheme == .dark ? .white : .black)
-                    }
-                    
-                    Button(action: {isProfileView = true}) {
-                        Image(systemName: "person.circle.fill")
-                            .resizable()
-                            .frame(width: 30, height: 30)
-                            .padding(.trailing, 16)
-                            .foregroundColor(colorScheme == .dark ? .white : .black)
-                    }
+        VStack {
+            HStack {
+                Button(action: {}) {
+                    Image(systemName: "gear")
+                        .resizable()
+                        .frame(width: 30.0, height: 30.0)
+                        .foregroundColor(colorScheme == .dark ? .white : .black)
                 }
-                .padding(.leading, 15.0)
-                .frame(height: 30.0)
-        
-                .sheet(isPresented: $isProfileView) {
-                    ProfileView(isAuthenticated: $isAuthenticated) //add binding after
+                Spacer()
+
+                Button(action: {}) {
+                    Image(systemName: "bell.fill")
+                        .resizable()
+                        .frame(width: 30.0, height: 30.0)
+                        .foregroundColor(colorScheme == .dark ? .white : .black)
                 }
 
-        Spacer()
+                Button(action: { isProfileView = true }) {
+                    Image(systemName: "person.circle.fill")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .padding(.trailing, 16)
+                        .foregroundColor(colorScheme == .dark ? .white : .black)
+                }
+            }
+            .padding(.leading, 15.0)
+            .frame(height: 30.0)
 
-        
-        //tabs
-        TabView(selection: $selectedTab) {
-            TutorNowView()
-                .tabItem {
-                    Image(systemName: "graduationcap.fill")
-                    Text("Tutoring")
-                }.tag(0)
-            
-            ListingsView()
-                .tabItem {
-                    Image(systemName: "list.bullet")
-                    Text("Listings")
-                }.tag(1)
-            
-            ChatsView()
-                .tabItem {
-                    Image(systemName: "message.fill")
-                    Text("Chats")
-                }.tag(2)
-        }
-        .onAppear {
-            selectedTab=1
-        }
-        
-       
+            .sheet(isPresented: $isProfileView) {
+                ProfileView(isAuthenticated: $isAuthenticated)
+            }
 
+            // tabs
+            TabView(selection: $selectedTab) {
+                TutorNowView()
+                    .tabItem {
+                        Image(systemName: "graduationcap.fill")
+                        Text("Tutoring")
+                    }.tag(0)
+
+                ListingsView()
+                    .tabItem {
+                        Image(systemName: "list.bullet")
+                        Text("Listings")
+                    }.tag(1)
+
+                ChatsView()
+                    .tabItem {
+                        Image(systemName: "message.fill")
+                        Text("Chats")
+                    }.tag(2)
+            }
+            .onAppear {
+                selectedTab = 1
+            }
+        }
     }
 }
 
-
-
-#Preview{
+#Preview {
     ContentView()
 }
--- MVP_Tutors/MVP_TutorsApp.swift
+++ MVP_Tutors/MVP_TutorsApp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import FirebaseCore
+import Combine
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
@@ -17,31 +18,33 @@ class AppDelegate: NSObject, UIApplicationDelegate {
   }
 }
 
+class AppViewModel: ObservableObject {
+    @Published var isAuthenticated = false
+}
+
 
 
 @main
 struct MVP_TutorsApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    @AppStorage("isAuthenticated") private var isAuthenticated = false // Store authentication state
+    @StateObject private var appViewModel = AppViewModel()
 
     var body: some Scene {
         WindowGroup {
-            if isAuthenticated {
+            if appViewModel.isAuthenticated {
                 ContentView()
             } else {
-                AuthView(isAuthenticated: $isAuthenticated)
+                AuthView(isAuthenticated: $appViewModel.isAuthenticated)
             }
-        }
-    }
-
-    init() {
-        // Initialize isAuthenticated to false if it's not set
-        if UserDefaults.standard.value(forKey: "isAuthenticated") == nil {
-            isAuthenticated = false
         }
     }
 }
 
+
+//init() {
+// Initialize isAuthenticated to false if it's not set
+//if UserDefaults.standard.value(forKey: "isAuthenticated") == nil
+    //isAuthenticated = false
 
 
 
