2b3685b8f41972d97ea035e05b15955e291ab41a
Manas Nagelia <84251015+Manas-Nagelia@users.noreply.github.com>
2023-10-23 21:10:12 -0400

    Added photos picker functionality

---
+++ MVP_Tutors/AvatarView.swift
@@ -0,0 +1,18 @@
+//
+//  AvatarView.swift
+//  MVP_Tutors
+//
+//  Created by Manas Nagelia on 10/23/23.
+//
+
+import SwiftUI
+
+struct AvatarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AvatarView()
+}
--- MVP_Tutors/ProfileView.swift
+++ MVP_Tutors/ProfileView.swift
@@ -1,12 +1,14 @@
 import SwiftUI
+import PhotosUI
 import Firebase
 import FirebaseAuth
 //Note: Fix Logout so that it exits out into the login view
 struct ProfileView: View {
     @Environment(\.presentationMode) var presentationMode
     @Binding var isAuthenticated: Bool
+    @State private var selection: PhotosPickerItem?
     @State private var user: User?
-
+    
     var body: some View {
         VStack {
             if let user = $user.wrappedValue {
@@ -14,15 +16,32 @@ struct ProfileView: View {
                     .resizable()
                     .frame(width: 100, height: 100)
                     .clipShape(Circle())
-                    .padding()
-
+                
+                /*
+                 Button(action: {
+                 openFilePicker()
+                 }) {
+                 Text("Edit Avatar")
+                 .padding(.top, 5)
+                 .padding(.bottom, 5)
+                 }
+                 */
+                
+                PhotosPicker(
+                    selection: $selection,
+                    matching: .images
+                ) {
+                    Text("Edit Avatar")
+                        .padding(.top, 5)
+                        .padding(.bottom, 5)
+                }
+                
+                
+                
                 Text(user.displayName ?? "Display Name")
                     .font(.title)
-
-                Text(user.email ?? "Email Address")
-                    .font(.subheadline)
-                    .padding()
-
+                
+                
                 Button("Logout") {
                     do {
                         try Auth.auth().signOut()
@@ -34,27 +53,31 @@ struct ProfileView: View {
                         print("Error signing out: \(error.localizedDescription)")
                     }
                 }
-                .frame(maxWidth: .infinity)
+                .frame(maxWidth: 100)
                 .padding()
                 .background(Color.blue)
                 .foregroundColor(.white)
                 .cornerRadius(10)
             } else{
-                Text(String("Is Autheticated: " + String(isAuthenticated)))
+                Text(String("Is Authenticated: " + String(isAuthenticated)))
             }
         }
         .onAppear {
             fetchUserData()
         }
     }
-
+    
     func fetchUserData() {
         if let currentUser = Auth.auth().currentUser {
             user = currentUser
         }
     }
-
+    
     init(isAuthenticated: Binding<Bool>) {
         self._isAuthenticated = isAuthenticated
     }
+}
+
+#Preview{
+    ProfileView(isAuthenticated: .constant(true))
 }
