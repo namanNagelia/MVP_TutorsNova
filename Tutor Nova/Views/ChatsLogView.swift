664ed085b2232cf62ce739c3c1e15f3ce7882eca
namanNagelia <nagelia.naman@gmail.com>
2023-11-23 10:51:37 -0500

    changes

---
+++ Tutor Nova/Views/ChatsLogView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatsLogView.swift
+//  Tutor Nova
+//
+//  Created by Naman Nagelia on 11/22/23.
+//
+
+import SwiftUI
+
+struct ChatsLogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatsLogView()
+}
